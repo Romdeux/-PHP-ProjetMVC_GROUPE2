2d821621aa463e3935428bb181a0dcf2fd1dc401
SimonRobert13 <SimonRobert13@users.noreply.github.com>
2017-10-27 17:09:28 +0200

    Add files via upload

--- Srcs/actions/AddSurveyAction.inc.php
+++ Srcs/actions/AddSurveyAction.inc.php
@@ -27,6 +27,41 @@ class AddSurveyAction extends Action {
 	 */
 	public function run() {
 		/* TODO START */
+        $question = htmlentities($_POST['questionSurvey']);
+          
+        for ($i = 1; $i <= (count($_POST)-1) ; $i++){
+            $Response[$i] = htmlentities($_POST['responseSurvey'.$i]);
+        }
+        
+         var_dump($_POST);
+        
+        if ($this->getSessionLogin()===null) {
+			$this->setMessageView("Vous devez être authentifié.", "alert-error");
+			return;
+		}
+        else {
+            if($question!=''){
+                if($_POST['responseSurvey1']=='' && $_POST['responseSurvey2']==''){
+                    $this->setMessageView("Il faut saisir au moins 2 réponses.");
+                }
+                else{
+                    
+                    $survey=new Survey($_SESSION['login'],$question);
+                    $this->database->saveSurvey($survey);
+                    for($i=1 ; $i<=5; $i++){
+                        if($_POST['responseSurvey'.$i]!=''){
+                            $tampon=new Response($question, $Response[$i],$i);
+                            $this->database->saveResponse($tampon);
+                        }
+                    }
+                    $this->setMessageView("Merci, nous avons ajouté votre sondage");
+                }
+            }
+            else{
+                $this->setMessageView("La question est obligatoire");
+            }
+        }
+		
 		/* TODO END */
 	}
 
