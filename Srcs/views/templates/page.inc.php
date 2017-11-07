b6984af30bd1a02746dde5563b44a409a47878e0
Romain <iaco2702@gmail.com>
2017-11-07 11:58:38 +0100

    Update page.inc.php

--- Srcs/views/templates/page.inc.php
+++ Srcs/views/templates/page.inc.php
@@ -4,6 +4,8 @@
 		<meta charset="utf-8">
 		<title>Sondages</title>
 		<link rel="stylesheet" type="text/css" href="bootstrap.min.css" />
+	        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"/>
+
 </head>
 
 <body>
