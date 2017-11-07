d99e4bbb198ee112f32820c8d4e4d0542ba191a8
FJEANNOT <32481828+FJEANNOT@users.noreply.github.com>
2017-11-07 12:18:12 +0100

    Update page.inc.php

--- Srcs/views/templates/page.inc.php
+++ Srcs/views/templates/page.inc.php
@@ -3,8 +3,8 @@
   <head>
 		<meta charset="utf-8">
 		<title>Sondages</title>
-		<link rel="stylesheet" type="text/css" href="bootstrap.min.css" />
 	        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"/>
+		<link rel="stylesheet" type="text/css" href="bootstrap.min.css" />
 
 </head>
 
